10f9fd14ce6ee15f55ac5367153d2542fcc57909
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-03-30 21:24:27 +0700

    feat:update mock data

--- client/src/pages/shop/Homepage.tsx
+++ client/src/pages/shop/Homepage.tsx
@@ -3,56 +3,7 @@ import Breadcrumbs from '../../components/shop/Breadcrumbs';
 import FilterSidebar from '../../components/shop/FilterSidebar';
 import ProductSection from '../../components/shop/ProductSection';
 
-const DUMMY_PRODUCTS = [
-  {
-    id: '1',
-    name: 'Apple Macbook Pro (chip M1, 8GB RAM, 256GB SSD...',
-    price: 20000000,
-    image: 'https://images.unsplash.com/photo-1517336714460-d13d87704a42?w=400&q=80',
-    inStock: true,
-    proximity: '<1km',
-  },
-  {
-    id: '2',
-    name: 'MacBook Air M1 - 8GB/256GB - Fullbox',
-    price: 14200000,
-    image: 'https://images.unsplash.com/photo-1611186871348-b1ec696e52c9?w=400&q=80',
-    inStock: true,
-    proximity: 'Tại Campus BK',
-  },
-  {
-    id: '3',
-    name: 'Giáo trình Giải tích 1 + 2 (Bách Khoa)',
-    price: 50000,
-    image: 'https://images.unsplash.com/photo-1497633762265-9d179a990aa6?w=400&q=80',
-    inStock: true,
-    proximity: 'Cách bạn 500m',
-  },
-  {
-    id: '4',
-    name: 'Bàn học sinh viên gỗ ép - 1m2',
-    price: 350000,
-    image: 'https://images.unsplash.com/photo-1518455027359-f3f816b1a22a?w=400&q=80',
-    inStock: false,
-    proximity: 'Tại Campus Kinh tế',
-  },
-  {
-    id: '5',
-    name: 'Máy tính Casio fx-580VN X',
-    price: 450000,
-    image: 'https://images.unsplash.com/photo-1543286386-713bcd534bf5?w=400&q=80',
-    inStock: true,
-    proximity: 'Tại Campus Sư phạm',
-  },
-  {
-    id: '6',
-    name: 'Tai nghe Bluetooth Sony WH-1000XM4',
-    price: 3200000,
-    image: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=400&q=80',
-    inStock: true,
-    proximity: 'Cách bạn 1.5km',
-  },
-];
+import DUMMY_PRODUCTS from '../../data/products.json';
 
 const Homepage: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
