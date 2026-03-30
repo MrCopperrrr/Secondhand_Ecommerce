005a81a8c3b71a9967b3050be981088f0fd0a4da
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-03-30 19:28:52 +0700

    Add Product detail page

---
+++ client/src/pages/shop/ProductDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetail: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">STT 10: Chi tiết sản phẩm (FR-15)</h1>
+        </div>
+    );
+};
+
+export default ProductDetail;
