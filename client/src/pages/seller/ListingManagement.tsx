6d21372f3b1ceb507edf93cf1184b30940c1ce40
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-03-30 19:28:29 +0700

    Add Listing management page

---
+++ client/src/pages/seller/ListingManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListingManagement: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">STT 8: Quản lý sản phẩm (FR-10, 12)</h1>
+        </div>
+    );
+};
+
+export default ListingManagement;
