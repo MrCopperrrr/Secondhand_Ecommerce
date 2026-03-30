1f743798b594b877d886dc84a79045eae499d3a2
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-03-30 19:28:21 +0700

    Add Address & Payment page

---
+++ client/src/pages/profile/AddressPayment.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddressPayment: React.FC = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">STT 5: Địa chỉ & Thanh toán (FR-05, 06, 07)</h1>
+        </div>
+    );
+};
+
+export default AddressPayment;
