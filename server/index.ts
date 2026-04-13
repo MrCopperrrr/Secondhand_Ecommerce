54df0237fffc85c0db8810f23d27c5853a28356d
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-04-13 20:18:59 +0700

    fix cors

--- server/index.ts
+++ server/index.ts
@@ -13,6 +13,12 @@ import transactionRouter from './src/routes/transactions.routes.js'
 import adminRouter from './src/routes/admin.routes.js'
 const app = express()
 const port = process.env.PORT || 3001
+app.use(cors({
+  // Thay link này bằng link Static Site bạn nhận được từ Render
+  origin: ['http://localhost:3000', 'https://secondhand-ecommerce-client.onrender.com'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // Cho phép gửi cookie/header Authorization nếu cần
+}));
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
@@ -32,9 +38,3 @@ databaseService.connect().then(() => {
     console.error("Critical error starting server:", err)
 })
 
-app.use(cors({
-  // Thay link này bằng link Static Site bạn nhận được từ Render
-  origin: ['http://localhost:3000', 'https://secondhand-ecommerce-client.onrender.com'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Cho phép gửi cookie/header Authorization nếu cần
-}));
