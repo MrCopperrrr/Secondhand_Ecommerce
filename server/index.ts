44fbaa146c4e3619b73edfe778989114c97e0547
Nguyễn Tấn Phát <ntphat0802@gmail.com>
2026-04-13 22:57:15 +0700

    fix frontend link and cors

--- server/index.ts
+++ server/index.ts
@@ -15,11 +15,10 @@ const app = express()
 const port = process.env.PORT || 3001
 app.use(cors({
   // Thay link này bằng link Static Site bạn nhận được từ Render
-  origin: ['http://localhost:3000', 'https://secondhand-ecommerce-client.onrender.com'], 
+  origin: ['http://localhost:3000', 'https://uni2hand.onrender.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // Cho phép gửi cookie/header Authorization nếu cần
 }));
-app.use(cors())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use('/users', userRouter)
